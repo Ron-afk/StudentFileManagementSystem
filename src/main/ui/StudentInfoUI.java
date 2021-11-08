d4d0a0ba6ffe220b8e6cf63deabb7810560e7af5
rangxiao <xiaorang@student.ubc.ca>
2021-11-08 13:26:58 -0800

    Phase 3 closed user story update

--- src/main/ui/StudentInfoUI.java
+++ src/main/ui/StudentInfoUI.java
@@ -6,8 +6,6 @@ import model.Student;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
 import java.util.List;
 
 /**
